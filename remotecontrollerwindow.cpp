0f6f034071804931bfd51548b5eb20b4e3e99a81
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-01-06 19:08:58 +0000

    Allow Smart Program settings to be changed while running

--- remotecontrollerwindow.cpp
+++ remotecontrollerwindow.cpp
@@ -2068,7 +2068,7 @@ void RemoteControllerWindow::RunSmartProgram(SmartProgram sp)
     if (m_smartProgram->run())
     {
         ui->PB_StartSmartProgram->setText("Stop");
-        ui->PB_SmartSettings->setEnabled(false);
+        //ui->PB_SmartSettings->setEnabled(false);
         ui->TW_SmartProgram->setEnabled(false);
         ui->LW_SmartProgram->setEnabled(false);
         ui->PB_ReloadSmartCommands->setEnabled(false);
@@ -2108,7 +2108,7 @@ void RemoteControllerWindow::StopSmartProgram()
     }
 
     ui->PB_StartSmartProgram->setText("Start");
-    ui->PB_SmartSettings->setEnabled(true);
+    //ui->PB_SmartSettings->setEnabled(true);
     ui->TW_SmartProgram->setEnabled(true);
     ui->LW_SmartProgram->setEnabled(true);
     ui->PB_ReloadSmartCommands->setEnabled(true);
