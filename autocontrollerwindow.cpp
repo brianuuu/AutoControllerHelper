b76fedd50dc1e5dc767d7dae60182852f5aaaf1d
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-01-16 23:02:21 +0000

    Bump version to 5.1.1

--- autocontrollerwindow.cpp
+++ autocontrollerwindow.cpp
@@ -1,7 +1,7 @@
 #include "autocontrollerwindow.h"
 #include "ui_autocontrollerwindow.h"
 
-static const QString c_version = "5.1.0";
+static const QString c_version = "5.1.1";
 
 //---------------------------------------------------------------------------
 // Constructor
